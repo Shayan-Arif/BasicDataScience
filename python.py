64cad3cfd518e40a8f8db9c3642e8c6080cce739
Muhammad Shayan <21sw067@students.muet.edu.pk>
2023-09-29 21:26:14 +0100

    done with the jupiter notebooks

---
+++ python.py
@@ -0,0 +1,39 @@
+# write me a print command 
+print("Hello World")
+# write me a all operatiors commands and print them
+
+# working with operators
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
+print(2%2)
+print(2**2)  # it will give you the power of the number
+print(2//2) # it will give you the integer value
+
+# working with strings
+# write me a string with double quotes
+print("Hello World")
+# write me a string with single quotes
+print('how are you')
+# write me a string with triple quotes
+print("""Hello World
+      how are you
+      using the triple quotes""")
+
+#working with variables
+
+mangoes_basket = 5
+print(mangoes_basket)
+print(type(mangoes_basket))
+print(id(mangoes_basket))
+
+
+#taking input from user
+
+name = input("Enter your name: ")
+father_name = input("Enter your father name: ")
+age = input("Enter your age: ")
+
+print(name, "\n ",father_name, age)
+
